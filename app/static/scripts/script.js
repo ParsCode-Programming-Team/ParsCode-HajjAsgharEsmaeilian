95a7f687aa3727bb33ccf83f9f9599a38c7f5f0b
Muhammad amin Mumeni <m.amin.mumeni@gmail.com>
2025-03-10 15:40:40 +0330

    add functionality to show/hide content and adjust z-index for layout consistency

--- app/static/scripts/script.js
+++ app/static/scripts/script.js
@@ -1,3 +1,11 @@
+var con = document.querySelector('.content');
+con.style.display = 'none';
+
+function ShowContent() {
+    var div = document.querySelector('.content');
+    div.style.display = 'flex';
+}
+
 document.getElementById("icon-scroll").addEventListener("click", function() {
     window.scrollBy({
         top: window.innerHeight,
